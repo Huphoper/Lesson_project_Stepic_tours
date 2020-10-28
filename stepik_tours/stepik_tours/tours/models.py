fff68f24d4133abb7860ffe7e51007a4fb4ecdb6
Mihail Duke <Huphoper@gmail.com>
2020-10-28 16:36:09 +0500

    28.10.2020

--- stepik_tours/stepik_tours/tours/models.py
+++ stepik_tours/stepik_tours/tours/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Departure(models.Model):
+    shortname = models.CharField(max_length=10)
+    fullname = models.CharField(max_length=64)
+
+
+class Tour(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    departure = models.ForeignKey(Departure, on_delete=models.CASCADE())
+    picture = models.TextField()
+    price = models.IntegerField()
+    stars = models.IntegerField()
+    country = models.CharField(max_length=64)
+    nights = models.IntegerField()
+    date = models.DateField()
